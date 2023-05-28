9ae2715502cb9d8c2170d8899a379c674c1425b3
Nenad <kuzic_nenad@protonmail.com>
2023-05-28 15:42:42 +0200

    Initial app/Java commit
    Added dependencies:
    1. Spring security
    2. JPA
    3. MySQL
    4. Spring web
    5. DevTools

---
+++ energymanagementsystem/src/main/java/rs/energymanagementsystem/energymanagementsystem/EnergymanagementsystemApplication.java
@@ -0,0 +1,13 @@
+package rs.energymanagementsystem.energymanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnergymanagementsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnergymanagementsystemApplication.class, args);
+	}
+
+}
---
+++ energymanagementsystem/src/test/java/rs/energymanagementsystem/energymanagementsystem/EnergymanagementsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.energymanagementsystem.energymanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnergymanagementsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
