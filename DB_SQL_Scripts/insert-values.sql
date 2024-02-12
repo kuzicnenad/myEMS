6b15dda349bd66a8cd4e90a8d9fc9517cecf4aab
Nenad <kuzic_nenad@protonmail.com>
2024-02-12 11:19:22 +0100

    Update SQL script for ROOT user

--- DB_SQL_Scripts/insert-values.sql
+++ DB_SQL_Scripts/insert-values.sql
@@ -10,23 +10,22 @@ VALUES 	('Admin','Admin_FirstName','Admin_LastName','1'),
 ;
 
 INSERT INTO User(name, username, email, password)
-VALUES 	('Admin1', 'Admin1', 'admin_1@myems.com', '$2a$12$OJM5mRrY9gC61uhk5Dbtt.5xmcT0QF6loN6HnKSr9fqSWKwrqGFLC'), 	/* pass -> admin */
-		('User1', 'User1', 'user_1@myems.com', '$2a$12$C4brIjIkdcSF0dJBvyFsjesGqq19Spy0xGbKEPXax0NfD/j6MicrG'),
-        ('Admin2', 'Admin2', 'admin_2@myems.com', '$2a$12$OJM5mRrY9gC61uhk5Dbtt.5xmcT0QF6loN6HnKSr9fqSWKwrqGFLC'), 	/* pass -> admin */
-		('User2', 'User2', 'user_2@myems.com', '$2a$12$C4brIjIkdcSF0dJBvyFsjesGqq19Spy0xGbKEPXax0NfD/j6MicrG'),
-        ('Admin3', 'Admin3', 'admin_3@myems.com', '$2a$12$OJM5mRrY9gC61uhk5Dbtt.5xmcT0QF6loN6HnKSr9fqSWKwrqGFLC'), 	/* pass -> admin */
-		('User3', 'User3', 'user_3@myems.com', '$2a$12$C4brIjIkdcSF0dJBvyFsjesGqq19Spy0xGbKEPXax0NfD/j6MicrG');		/* pass -> user */
+VALUES 	('Root', 'Root', 'root@myems.com', '$2a$12$ayPaoeYCLYSN2eW6GvxbUOoR1GnVgVtxqCCH4NzJgQYF.gB3dzgpG'), 		/* pass -> root */
+		('Admin1', 'Admin1', 'admin_1@myems.com', '$2a$12$OJM5mRrY9gC61uhk5Dbtt.5xmcT0QF6loN6HnKSr9fqSWKwrqGFLC'), 	/* pass -> admin */
+		('User1', 'User1', 'user_1@myems.com', '$2a$12$C4brIjIkdcSF0dJBvyFsjesGqq19Spy0xGbKEPXax0NfD/j6MicrG');		/* pass -> user */
         
 INSERT INTO Role(name)
-VALUES	('ADMIN'),
+VALUES	('ROOT'),
+		('ADMIN'),
 		('USER');
         
 INSERT INTO users_roles(user_id, role_id)
 VALUES	(1,1),
 		(1,2),
-        (7,2),
-        (8,1),
-        (10,2);
+        (1,3),
+        (2,2),
+        (2,3),
+        (3,3);
 
 
 /* ---------- Generate devices ---------- */
@@ -38,18 +37,15 @@ VALUES	('device_one','2017-11-20','Serbia', 1),
 		('device_five','2022-07-05','Croatia', 1),
 		('device_six','2009-04-09','Slovenia', 1);
 
-
+/* This part is invalid, logics were improved from this.
 CALL addHashPassword(1,'admin111');
 CALL addHashPassword(2,'user111');
 CALL addHashPassword(3,'user222');
 
-
-
-
 SELECT user_login as uname,first_name as Name,last_name as Surname, u.time_stamp as userCreationDate, password_hash, p.time_stamp as psdAddDate FROM Users as u
 INNER JOIN Passwords as p
 ON u.user_id = p.user_id;
-
+*/
 
 /* --- Generate procedures to create initial data --- */
 CALL generateElectricityData();
@@ -86,12 +82,10 @@ truncate table Gas_History_Data;
 /* ---------- Clear alarms table ---------- */
 truncate table Alarm_Data;
 
-/* ---- Clear users and passwords table ----*/
-truncate table passwords;
-truncate table users;        
-truncate table Users_Roles;
-truncate table User;
+/* ---- Clear users and passwords table ----*/  
+truncate table users_roles;
 truncate table Role;
+truncate table User;
 
 /* ---- Clear users and passwords table ----*/
 truncate table Devices;
