c4c6cd5cb92343dd5a266f11f3811034e6f644a8
Nenad <kuzic_nenad@protonmail.com>
2024-02-12 15:50:08 +0100

    remove unnecessary imports

--- energymanagementsystem/src/main/java/rs/energymanagementsystem/energymanagementsystem/security/SecurityConfig.java
+++ energymanagementsystem/src/main/java/rs/energymanagementsystem/energymanagementsystem/security/SecurityConfig.java
@@ -1,8 +1,5 @@
 package rs.energymanagementsystem.energymanagementsystem.security;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,12 +8,10 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
 import rs.energymanagementsystem.energymanagementsystem.exceptions.ExceptionControllerAdvice;
 import rs.energymanagementsystem.energymanagementsystem.exceptions.LoginFailureHandler;
 
