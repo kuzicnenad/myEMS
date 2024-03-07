cabbbfe877f9d41dbc660559f255cff74628d129
Nenad <kuzic_nenad@protonmail.com>
2024-03-07 23:10:44 +0100

    Remove parentheses

--- energymanagementsystem/src/main/java/rs/energymanagementsystem/energymanagementsystem/configCustom/security/SecurityConfig.java
+++ energymanagementsystem/src/main/java/rs/energymanagementsystem/energymanagementsystem/configCustom/security/SecurityConfig.java
@@ -63,7 +63,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests((authorize) -> authorize
+                .authorizeHttpRequests(authorize -> authorize
                         //authorize.anyRequest().authenticated()
                         .requestMatchers(staticResources).permitAll()
                         .requestMatchers(LOGIN_URL).permitAll()
